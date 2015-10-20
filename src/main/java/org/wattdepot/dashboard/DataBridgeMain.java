a3ef0e8481aff23793fc389bc536847ea2d6f53f
Cam Moore <cammymoore07@gmail.com>
2015-10-20 10:29:55 -1000

    added power history task.

--- src/main/java/org/wattdepot/dashboard/DataBridgeMain.java
+++ src/main/java/org/wattdepot/dashboard/DataBridgeMain.java
@@ -57,6 +57,7 @@ public class DataBridgeMain {
     timer.schedule(new CurrentPowerTask(), 100, 15000);
     timer.schedule(new HourlyEnergyTask(), 500, 60 * 60 * 1000);
     timer.schedule(new SensorStatusTask(), 750, 60 * 60 * 1000);
+    timer.schedule(new HourlyPowerHistoryTask(), 1000, 60 * 60 * 1000);
     timer.schedule(new DailyEnergyTask(), 1500, 24 * 60 * 60 * 1000);
   }
 }
